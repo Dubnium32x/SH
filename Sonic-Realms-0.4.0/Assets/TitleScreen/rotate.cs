80e7c7c98c46ccb40ab003f16c7e23de3e425622
Birb64 <60531532+Birb64@users.noreply.github.com>
2020-12-23 00:01:10 -0600

    KORBY(paralax test)

---
+++ Sonic-Realms-0.4.0/Assets/TitleScreen/rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0,5,0,Space.Self);
+    }
+}

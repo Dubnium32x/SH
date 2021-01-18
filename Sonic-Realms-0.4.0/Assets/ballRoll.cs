18a0dc14a39080bc4b40b10527d15d1fc327ad59
Birb64 <60531532+Birb64@users.noreply.github.com>
2021-01-17 22:28:03 -0600

    balls

---
+++ Sonic-Realms-0.4.0/Assets/ballRoll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using SonicRealms.Core.Actors;
+
+namespace SonicRealms.Core.Moves
+{
+    public class ballRoll : MonoBehaviour
+    {
+        public Transform SpriteT;
+        public float HitR;
+        public float HitL;
+        void FixedUpdate()
+        {
+            SpriteT.Rotate(0,0,-GetComponent<HedgehogController>().GroundVelocity * 256 * Time.deltaTime, Space.Self);
+        }
+
+    }
+}

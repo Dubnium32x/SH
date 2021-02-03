a220d91969377ef26add8d52046c337474b34be8
Birb64 <60531532+Birb64@users.noreply.github.com>
2021-02-02 23:12:01 -0600

    if you want me to change anything else at this point, ping me.
    consider me as active as ender or noahg at this point.

--- Sonic-Realms-0.4.0/Assets/StartLevelPixel.cs
+++ Sonic-Realms-0.4.0/Assets/StartLevelPixel.cs
@@ -11,11 +11,15 @@ public class StartLevelPixel : MonoBehaviour
         
     }
     // Update is called once per frame
+    float timer;
     void Update()
     {
-        if(!Death.IsDead)
+        timer += Time.deltaTime;
+        if (!Death.IsDead && timer >= 2)
             GetComponent<RawImage>().color += Color.white * Time.deltaTime;
         else
-            GetComponent<RawImage>().color += Color.black * Time.deltaTime;
+        {
+            GetComponent<RawImage>().color += Color.black * (Color.clear / 2) * Time.deltaTime;
+        }
     }
 }

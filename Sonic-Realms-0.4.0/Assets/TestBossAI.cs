2f0f0ae5715b578657ac9da52f29a817c329ce90
Birb64 <60531532+Birb64@users.noreply.github.com>
2021-02-28 00:49:06 -0600

    a little boss update

---
+++ Sonic-Realms-0.4.0/Assets/TestBossAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TestBossAI : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0.001f * (transform.position.x - GameObject.FindGameObjectWithTag("Player").transform.position.x), 0, 0);
+    }
+}

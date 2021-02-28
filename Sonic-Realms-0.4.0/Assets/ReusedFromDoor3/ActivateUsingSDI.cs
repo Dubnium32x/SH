df71cd0cf56456528bb55cbd0c2b1adb43bee95d
Birb64 <60531532+Birb64@users.noreply.github.com>
2021-02-27 19:58:11 -0600

    Checkpoint and new debug

--- Sonic-Realms-0.4.0/Assets/ReusedFromDoor3/ActivateUsingSDI.cs
+++ Sonic-Realms-0.4.0/Assets/ReusedFromDoor3/ActivateUsingSDI.cs
@@ -1,43 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.IO;
-using System.Linq;
+﻿using UnityEngine;
 //made by Birb64, modified from door 3, for use of sonic harmony.
 public class ActivateUsingSDI : MonoBehaviour
 {
-    string otherThing;
-    public string CapitalizedPlacement;
-    public string value;
-    // Start is called before the first frame update
-    void Start()
-    {
-        ASavedValue(CapitalizedPlacement);
-    }
+    public string Word;
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(otherThing);
-        
-        if (otherThing != value)
-        {
-            gameObject.SetActive(false);
-        }
-        if (otherThing == value)
-        {
-            gameObject.SetActive(true);
-        }
-    }
-    public void ASavedValue(string Placement)
-    {
-        string path = Application.dataPath + "/SDI.H";
-        if (File.Exists(path))
-        {
-            foreach (string thing in File.ReadAllLines(path).Where(line => line.StartsWith(Placement)))
-            {
-                otherThing = thing;
-            }
-        }
+            gameObject.SetActive(SDR.DDVER(Word));
     }
 }
---
+++ Sonic-Realms-0.4.0/Assets/Teleport.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using SonicRealms.Core.Actors;
+
+public class Teleport : MonoBehaviour
+{
+    public Text[] TextX = new Text[0];
+    public Text[] TextY = new Text[0];
+    public Text[] TextZ = new Text[0];
+    public Text XYZ;
+    private bool IsPressed;
+
+    public void Pressed()
+    {
+        IsPressed = true;
+    }
+
+    void Update()
+    {
+
+        #region XYZ changer
+        XYZ.text = "X: " + GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.x.ToString() + "\nY: " + GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.y.ToString() + "\nZ: " + GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.z.ToString();
+        #endregion
+        float X = float.Parse(TextX[0].text) != 0 ? float.Parse(TextX[0].text): 0;
+        float Y = float.Parse(TextY[0].text) != 0 ? float.Parse(TextY[0].text): 0;
+        float Z = float.Parse(TextZ[0].text) != 0 ? float.Parse(TextZ[0].text): 0;
+        if (!GameObject.Find("RelativeToggle").GetComponent<Toggle>().isOn)
+        {
+            #region Simplicities
+            if (X == 0 && Z == 0 && Y != 0)
+            {
+                Y = GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.y;
+            }
+            if (Y == 0 && Z == 0 && X != 0)
+            {
+                X = GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.y;
+            }
+            if (X == 0 && Y == 0 && Z != 0)
+            {
+                Z = GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.y;
+            }
+            #endregion}
+        }
+        if (IsPressed)
+        {
+            if(!GameObject.Find("RelativeToggle").GetComponent<Toggle>().isOn)
+            GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position = new Vector3(X, Y, Z);
+            else
+                GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position = new Vector3(GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.x + X, GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.y + Y, GameObject.FindGameObjectWithTag("Player").GetComponent<HedgehogController>().transform.position.z + Z);
+            IsPressed = false;
+
+        }
+    }
+}
